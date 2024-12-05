6eb28e517142b4fa00be06bb34a0ce63d567ef19
SJarod <jarsengkeoPro@gmail.com>
2024-12-05 22:06:09 +0100

    first commit

---
+++ Source/PlayerCharactersUE/PlayerCharactersUEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlayerCharactersUEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYERCHARACTERSUE_API APlayerCharactersUEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
